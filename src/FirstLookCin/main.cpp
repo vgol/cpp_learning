cd6b611842b3050d91f576a2111230602e43f4e3
Vladimir Golubkov <vgol.mail@gmail.com>
2017-02-13 00:01:52 +0300

    playing with iostream

---
+++ src/FirstLookCin/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    cout << "You've entered: " << number << endl;
+    return 0;
+}
--- src/HelloWorld/main.cpp
+++ src/HelloWorld/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 
-int main() {
+int main()
+{
     std::cout << "Hello, world!" << std::endl;
     return 0;
 }
