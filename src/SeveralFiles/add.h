8102bfc59536f9196b5f0fcc303a1a1137c8dccf
Vladimir Golubkov <vgol.mail@gmail.com>
2017-02-23 14:15:08 +0300

    add first program wit multiple source files

---
+++ src/SeveralFiles/add.h
@@ -0,0 +1,7 @@
+// Header guard (prevent header file from being included multiple times.
+#ifndef CPP_LEARNING_ADD_H
+#define CPP_LEARNING_ADD_H
+
+int add(int x, int y);
+
+#endif //CPP_LEARNING_ADD_H
