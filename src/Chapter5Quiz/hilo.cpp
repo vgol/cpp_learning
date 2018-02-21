d69ac231ec53a241403ff61d5208473866fd6cbf
Vladimir Golubkov <vgol.mail@gmail.com>
2018-02-21 23:41:31 +0300

    chapter 5 quiz (hilo play).

---
+++ src/Chapter5Quiz/hilo.cpp
@@ -0,0 +1,88 @@
+#include <random>
+#include <iostream>
+#include <ctime>
+#include "hilo.h"
+
+
+int main()
+{
+    hilo::playMultipleTimes();
+    // hilo::playHiLo();
+
+    return 0;
+}
+
+int hilo::getRandomNumber(int min, int max)
+{
+    // Should work but don't due to compiler's bug (they say so...).
+    // static std::random_device rd;
+    // static std::mt19937 mersenne{rd()};
+
+    // initialize our mersenne twister with a random seed
+    static std::mt19937 mersenne(static_cast<unsigned int>(time(nullptr)));
+    static const double fraction = 1.0 / (static_cast<double>(mersenne.max()) + 1.0);
+    return min + static_cast<int>((max - min + 1) * (mersenne() * fraction));
+}
+
+void hilo::playHiLo()
+{
+    auto key = getRandomNumber(hilo::min, hilo::max);
+
+    std::cout << "Let's play a game.  I'm thinking of a number."
+            "  You have 7 tries to guess what it is.\n";
+
+    for (int guessNum = 0; guessNum <= 7; ++guessNum)
+    {
+        std::cout << "Guess #" << guessNum << ": ";
+        auto guess = getIntNum();
+
+        if (guess == key)
+        {
+            std::cout << "Correct! You win!\n";
+            return;
+        }
+        else if (guess > key)
+        {
+            std::cout << "Your guess is too high.\n";
+        }
+        else
+        {
+            std::cout << "Your guess is too low.\n";
+        }
+    }
+
+    std::cout << "Sorry, you lose.  The correct number was " << key << ".\n";
+}
+
+int hilo::getIntNum()
+{
+    int guess;
+    std::cin >> guess;
+    return guess;
+}
+
+char hilo::getYesOrNo()
+{
+    char answer(0);
+    do
+    {
+        std::cout << "Would you like to play again (y/n)? ";
+        std::cin >> answer;
+    }
+    while (not (answer == 'y' or answer == 'n' or answer == 'Y' or answer == 'N'));
+
+    return answer;
+}
+
+void hilo::playMultipleTimes()
+{
+    char answer(0);
+    do
+    {
+        playHiLo();
+        answer = getYesOrNo();
+    }
+    while (answer == 'y' or answer == 'Y');
+
+    std::cout << "Thank you for playing.\n";
+}
---
+++ src/Chapter5Quiz/hilo.h
@@ -0,0 +1,21 @@
+#ifndef CPP_LEARNING_HILO_H
+#define CPP_LEARNING_HILO_H
+
+namespace hilo
+{
+const int min(1);
+const int max(100);
+
+int getRandomNumber(int min, int max);
+
+void playHiLo();
+
+void playMultipleTimes();
+
+int getIntNum();
+
+char getYesOrNo();
+
+} // hilo
+
+#endif //CPP_LEARNING_HILO_H
