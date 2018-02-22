a4bbfee388d02b59ca6f68158712f6e09eb0e303
Vladimir Golubkov <vgol.mail@gmail.com>
2018-02-23 01:19:13 +0300

    cool quiz about stupid sorting algorithm! :)

--- src/SortingArrays/bubble.cpp
+++ src/SortingArrays/bubble.cpp
@@ -8,16 +8,25 @@ int main()
 {
     const int length(9);
     int array[length] = { 6, 3, 2, 9, 7, 1, 5, 4, 8 };
+    int sortedFromTheEnd(1);
 
     for (int iter = 0; iter < length - 1; ++iter)
     {
-        for (int inx = 0; inx < length - 1; ++inx)
+        bool something_was_swapped(false);
+
+        for (int inx = 0; inx < length - sortedFromTheEnd; ++inx)
         {
             if (array[inx] > array[inx + 1])
             {
                 std::swap(array[inx], array[inx + 1]);
+                something_was_swapped = true;
             }
         }
+        if (not something_was_swapped){
+            std::cout << "\nIt is already sorted! Iteration: " << iter << '\n';
+            break;
+        }
+        ++sortedFromTheEnd;
     }
 
     printArray(array, length);
